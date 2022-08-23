24f2bb1ce5d469c0471fa304f3c7d8bda893dc85
Francis <mufraggie@gmail.com>
2022-08-23 05:22:15 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", () =>{
+  document.addEventListener("submit", function(event){
+    event.preventDefault();
+  });
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", addTask);
+
+
+function addTask(e){
+  e.preventDefault();
+  const task = e.target.querySelector('#new-task-description').value;
+  let listTask = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete)
+  btn.textContent= "X";
+  listTask.innerText = `${task} `;
+  document.getElementById('tasks').appendChild(listTask);
+  listTask.appendChild(btn);
+  e.target.querySelector('#new-task-description').value = '';
+}} )
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
